acb59af9e472028935f5f80494a310a879311876
rohscx <R_E__g_@Macintosh.local>
2017-11-05 11:58:12 -0500

    setup

---
+++ Dockerfile
@@ -0,0 +1,59 @@
+# Build:
+# docker build -t nodejs .
+#
+# Run:
+# docker run -it nodejs
+#
+# Compose:
+# docker-compose up -d
+
+FROM ubuntu:latest
+MAINTAINER RoHscx
+
+
+# 80 = HTTP, 443 = HTTPS, 3000 = Meteor.JS server 8080 = node-inspector
+EXPOSE 80 8080 443 3000
+
+
+# Set development environment as default
+ENV NODE_ENV development
+
+
+# Install Utilities
+RUN apt-get update \
+&& apt-get install -qy curl \
+ locales \
+ git \
+ apt-utils\
+ build-essential \
+ vim \
+ npm
+RUN apt-get clean \
+&& rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/* \
+&& locale-gen en_US.UTF-8
+
+
+# Install Nodejs on Ubuntu systems
+RUN curl -sL https://deb.nodesource.com/setup_8.x | bash -
+RUN apt-get install -y nodejs
+
+# Install mongodb on Ubuntu Systems
+RUN apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6 \
+&& echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+RUN apt-get install -y mongodb-org
+
+# Run Entrypoint script
+COPY docker-entrypoint.sh /docker-entrypoint.sh
+ENTRYPOINT [ "/docker-entrypoint.sh" ]
+RUN chmod 755 /docker-entrypoint.sh
+
+
+# Add Meteor user
+RUN adduser --disabled-password --gecos "" node_dev
+USER node_dev
+RUN cd ~/ \
+&& mkdir nodeProjects
+WORKDIR /home/node_dev/nodeProjects
+
+# Run bash
+CMD [ "-s" ]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,87 @@
+#!/bin/bash -e
+# =====================================================================
+# Build script running NodeJS & mongoDB in Docker environment
+#
+# Source:
+# Web:
+#
+# =====================================================================
+
+START_DELAY=2
+
+NODE_HOME=/home/node/
+NODE_PROJECT_NAME=generalAPI
+NODE_PROJECT_HOME=/home/node/generalAPI
+NODE_PROJECT_APP=/home/node/generalAPI
+NODE_PROJECT_CD="cd ~/generalAPI"
+NODE_PROJECT_GIT_PULL="git pull"
+NODE_PROJECT_START="meteor --settings settings.json debug > logs/stdout.log 2> logs/stderr.log"
+NODE_FAKEOUT=~/tuoekaf
+
+
+# Error codes
+E_ILLEGAL_ARGS=126
+
+# Help function used in error messages and -h option
+usage() {
+  echo ""
+  echo "Docker entry script for Meteor service container"
+  echo ""
+  echo "-f: Start Node in foreground with existing configuration."
+  echo "-h: Show this help."
+  echo "-s: Initialize environment like -i and start Meteor in foreground." !!!!!
+  echo ""
+}
+
+
+initConfig() {
+  if [ ! "$(ls --ignore .keys --ignore .authoritative --ignore .recursive --ignore -A ${NODE_FAKEOUT})"  ]; then
+    git clone https://github.com/rohscx/generalAPI.git
+  else
+    echo "Node configuration already initialized........."
+  fi
+}
+
+
+start() {
+  bash
+}
+
+# Evaluate arguments for build script.
+if [[ "${#}" == 0 ]]; then
+  usage
+  exit ${E_ILLEGAL_ARGS}
+fi
+
+# Evaluate arguments for build script.
+while getopts fhis flag; do
+  case ${flag} in
+    f)
+      start
+      exit
+      ;;
+    h)
+      usage
+      exit
+      ;;
+    s)
+      initConfig
+      start
+      exit
+      ;;
+    *)
+      usage
+      exit ${E_ILLEGAL_ARGS}
+      ;;
+  esac
+done
+
+# Strip of all remaining arguments
+shift $((OPTIND - 1));
+
+# Check if there are remaining arguments
+if [[ "${#}" > 0 ]]; then
+  echo "Error: To many arguments: ${*}."
+  usage
+  exit ${E_ILLEGAL_ARGS}
+fi
